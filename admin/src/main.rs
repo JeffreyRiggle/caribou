ea41575fa70cb79442c19180e1143b954797e55d
Jeff <JeffreyRiggle@gmail.com>
2023-12-11 22:13:22 -0500

    feat(admin): Add ability to update domain status

--- admin/src/main.rs
+++ admin/src/main.rs
@@ -1,12 +1,17 @@
-use actix_web::{get, App, web, Result, HttpServer, Responder};
-use rusqlite::Connection;
-use serde::Serialize;
+use actix_web::{get, put, App, web, Result, HttpServer, Responder};
+use rusqlite::{Connection};
+use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize)]
 struct DomainData {
     domain: String,
     status: String,
-    download_assets: String,
+    downloadAssets: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct DomainStatus {
+    status: String
 }
 
 #[derive(Serialize)]
@@ -22,7 +27,7 @@ async fn get_domains() -> Result<impl Responder> {
         Ok(DomainData {
             domain: row.get(0)?,
             status: row.get(1)?,
-            download_assets: row.get(2)?
+            downloadAssets: row.get(2)?
         })
     }).unwrap();
 
@@ -36,11 +41,24 @@ async fn get_domains() -> Result<impl Responder> {
     }))
 }
 
+#[put("/domains/{domain}/status")]
+async fn update_domain_status(domain: web::Path<String>, update: web::Json<DomainStatus>) -> Result<impl Responder> { 
+    let conn = Connection::open("../grepper.db").unwrap();
+    let mut stmt = conn.prepare("UPDATE domains SET status = ?1 WHERE domain = ?2").unwrap();
+    stmt.execute((update.status.clone(), domain.as_str())).unwrap();
+    Ok(web::Json(DomainData {
+        domain: String::from(domain.as_str()),
+        status: String::from(&update.status),
+        downloadAssets: String::from("")
+    }))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(get_domains)
+            .service(update_domain_status)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
