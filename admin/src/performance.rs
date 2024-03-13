fd6ed9ea96d2dfa561b6299d166ccbef96c0e298
Jeffrey Riggle <jeffreyriggle@Jeffreys-MBP.lan>
2024-03-12 23:24:24 -0400

    feat(admin): Get more details on performance page

--- admin/src/performance.rs
+++ admin/src/performance.rs
@@ -1,5 +1,10 @@
 use rusqlite::{Connection, Error};
 
+pub struct PerformancePageResult {
+    pub bytes: u64,
+    pub url: String,
+}
+
 pub fn get_total_pages() -> Result<u32, Error> {
     let conn = match Connection::open("../grepper.db") {
         Ok(c) => c,
@@ -11,3 +16,100 @@ pub fn get_total_pages() -> Result<u32, Error> {
     let mut stmt = conn.prepare("SELECT COUNT(*) FROM resources").unwrap();
     stmt.query_row([], |row| row.get(0))
 }
+
+pub fn get_total_processed_pages() -> Result<u32, Error> {
+    let conn = match Connection::open("../grepper.db") {
+        Ok(c) => c,
+        Err(e) => {
+            println!("Failed to create connection {}", e);
+            panic!("Failed to connect to database")
+        }
+    };
+    let mut stmt = conn.prepare("SELECT COUNT(*) FROM resources WHERE status = 'Processed'").unwrap();
+    stmt.query_row([], |row| row.get(0))
+}
+
+pub fn get_last_run_time() -> Result<f64, Error> {
+    let conn = match Connection::open("../grepper.db") {
+        Ok(c) => c,
+        Err(e) => {
+            println!("Failed to create connection {}", e);
+            panic!("Failed to connect to database")
+        }
+    };
+    let mut stmt = conn.prepare("SELECT MIN(lastIndex) from resources").unwrap();
+    stmt.query_row([], |row| row.get(0))
+}
+
+pub fn get_max_js() -> Result<PerformancePageResult, Error> {
+    let conn = match Connection::open("../grepper.db") {
+        Ok(c) => c,
+        Err(e) => {
+            println!("Failed to create connection {}", e);
+            panic!("Failed to connect to database")
+        }
+    };
+    let mut stmt = conn.prepare("SELECT max(jsBytes), url from metadata").unwrap();
+    stmt.query_row([], |row| Ok(PerformancePageResult { bytes: row.get(0).unwrap(), url: row.get(1).unwrap() }))
+}
+
+pub fn get_average_js() -> Result<f64, Error> {
+    let conn = match Connection::open("../grepper.db") {
+        Ok(c) => c,
+        Err(e) => {
+            println!("Failed to create connection {}", e);
+            panic!("Failed to connect to database")
+        }
+    };
+    let mut stmt = conn.prepare("SELECT AVG(jsBytes) from metadata").unwrap();
+    stmt.query_row([], |row| row.get(0))
+}
+
+pub fn get_max_css() -> Result<PerformancePageResult, Error> {
+    let conn = match Connection::open("../grepper.db") {
+        Ok(c) => c,
+        Err(e) => {
+            println!("Failed to create connection {}", e);
+            panic!("Failed to connect to database")
+        }
+    };
+    let mut stmt = conn.prepare("SELECT max(cssBytes), url from metadata").unwrap();
+    stmt.query_row([], |row| Ok(PerformancePageResult { bytes: row.get(0).unwrap(), url: row.get(1).unwrap() }))
+}
+
+pub fn get_average_css() -> Result<f64, Error> {
+    let conn = match Connection::open("../grepper.db") {
+        Ok(c) => c,
+        Err(e) => {
+            println!("Failed to create connection {}", e);
+            panic!("Failed to connect to database")
+        }
+    };
+    let mut stmt = conn.prepare("SELECT AVG(cssBytes) from metadata").unwrap();
+    stmt.query_row([], |row| row.get(0))
+}
+
+pub fn get_max_html() -> Result<PerformancePageResult, Error> {
+    let conn = match Connection::open("../grepper.db") {
+        Ok(c) => c,
+        Err(e) => {
+            println!("Failed to create connection {}", e);
+            panic!("Failed to connect to database")
+        }
+    };
+    let mut stmt = conn.prepare("SELECT max(htmlBytes), url from metadata").unwrap();
+    stmt.query_row([], |row| Ok(PerformancePageResult { bytes: row.get(0).unwrap(), url: row.get(1).unwrap() }))
+}
+
+pub fn get_average_html() -> Result<f64, Error> {
+    let conn = match Connection::open("../grepper.db") {
+        Ok(c) => c,
+        Err(e) => {
+            println!("Failed to create connection {}", e);
+            panic!("Failed to connect to database")
+        }
+    };
+    let mut stmt = conn.prepare("SELECT AVG(htmlBytes) from metadata").unwrap();
+    stmt.query_row([], |row| row.get(0))
+}
+
--- admin/src/views.rs
+++ admin/src/views.rs
@@ -1,8 +1,9 @@
 use actix_web::{get, HttpResponse};
 use tera::{Context, Tera};
 use lazy_static::lazy_static;
+use crate::performance::{get_average_css, get_average_html, get_average_js, get_last_run_time, get_max_css, get_max_html, get_max_js, get_total_pages, get_total_processed_pages};
+
 use super::domain::get_domains;
-use super::performance::get_total_pages;
 
 lazy_static! {
     pub static ref TEMPLATES: Tera = {
@@ -54,6 +55,22 @@ async fn get_domain_management_page() -> HttpResponse {
 async fn get_performance_page() -> HttpResponse {
     let mut context = Context::new();
     context.insert("totalPages", &get_total_pages().unwrap());
+    context.insert("processedPages", &get_total_processed_pages().unwrap());
+    context.insert("lastRun", &get_last_run_time().unwrap());
+    let max_js_result = get_max_js().unwrap();
+    context.insert("maxJsSize", &bytes_to_display(max_js_result.bytes as f64));
+    context.insert("maxJsResource", &max_js_result.url);
+    context.insert("averageJs", &bytes_to_display(get_average_js().unwrap()));
+    
+    let max_css_result = get_max_css().unwrap();
+    context.insert("maxCssSize", &bytes_to_display(max_css_result.bytes as f64));
+    context.insert("maxCssResource", &max_css_result.url);
+    context.insert("averageCss", &bytes_to_display(get_average_css().unwrap()));
+
+    let max_html_result = get_max_html().unwrap();
+    context.insert("maxHtmlSize", &bytes_to_display(max_html_result.bytes as f64));
+    context.insert("maxHtmlResource", &max_html_result.url);
+    context.insert("averageHtml", &bytes_to_display(get_average_html().unwrap()));
 
     let page = match TEMPLATES.render("performance.html", &context) {
         Ok(p) => p.to_string(),
@@ -68,3 +85,18 @@ async fn get_performance_page() -> HttpResponse {
        .body(page)
 }
 
+fn bytes_to_display(bytes: f64) -> String {
+    if bytes < 1000f64 {
+        return bytes.to_string() + "B";
+    }
+
+    if bytes < 1000000f64 {
+        return (bytes / 1000f64).to_string() + "Kb";
+    }
+
+    if bytes < 1000000000f64 {
+        return (bytes / 1000000f64).to_string() + "Mb";
+    }
+
+    (bytes / 1000000000f64).to_string() + "Gb"
+}
