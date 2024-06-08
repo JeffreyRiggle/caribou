0cefbd133ec82d3e1007530d5c898e9256101194
Jeffrey Riggle <jeffreyriggle@gmail.com>
2024-06-07 23:38:02 -0400

    feat(grepper): Add background stars to graph page

--- grepper/static/grapher.js
+++ grepper/static/grapher.js
@@ -1,4 +1,8 @@
 const possibleColors = ['blue', 'red', 'yellow', 'orange', 'purple'];
+let canvasWidth, canvasHeight;
+const searchResults = [];
+const stars = [];
+
 function handleChange(e) {
   fetch('/query-graph?q=' + e.target.value).then(res => {
 		  return res.json().then(j => buildResults(j.results));
@@ -8,6 +12,16 @@ function handleChange(e) {
 addEventListener('load', () => {
 		const input = document.getElementById('search');
 		input.addEventListener('change', handleChange);
+		const canvas = document.getElementById('result-view');
+		canvasWidth = canvas.width = window.innerWidth;
+		canvasHeight = canvas.height = window.innerHeight - 100;
+
+		const totalStars = Math.floor(Math.random() * 100);
+		for (let i = 0; i < totalStars; i++)
+		{
+				stars.push(new Star());
+		}
+		requestAnimationFrame(runAnimationLoop);
 });
 
 function getRelativeSize(items) {
@@ -20,6 +34,41 @@ function getFillGradient(context, x, y, innerRadius, relativeRadius, color, colo
 		gradient.addColorStop(colorStop, color);
 		gradient.addColorStop(1, 'white');
 		return gradient;
+}
+
+class Star {
+		constructor() {
+				this.x = Math.random() * canvasWidth;
+				this.y = Math.random() * canvasHeight;
+				this.size = Math.random() + .05;
+				this.blinkChance = .003;
+				this.alpha = 1;
+				this.alphaChange = 0;
+		}
+
+		draw = (context) => {
+				context.beginPath();
+				context.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+				context.fillStyle = `rgba(255, 255, 255, ${this.alpha})`;
+				context.fill();
+				context.closePath();
+		}
+
+		update = () => {
+				if (this.alphaChange === 0 && Math.random() < this.blinkChance) {
+						this.alphaChange = -1;
+						return;
+				}
+
+				if (this.alphaChange === 0) return;
+
+				this.alpha += this.alphaChange * .05;
+				if (this.alpha <= 0) {
+						this.alphaChange = 1;
+				} else if (this.alpha >= 1) {
+						this.alphaChange = 0;
+				}
+		}
 }
 
 class SearchResult {
@@ -75,22 +124,20 @@ class SearchResult {
 		}
 }
 
-const searchResults = [];
-let animationLoopStarted = false;
-
 function runAnimationLoop() {
-		if (searchResults.length < 1) {
-				requestAnimationFrame(runAnimationLoop);
-				return;
-		}
-
 		const canvas = document.getElementById('result-view');
 		const context = canvas.getContext('2d');
 		
 		context.clearRect(0, 0, canvas.width, canvas.height);
 
-		searchResults.forEach(r => r.update());
-		searchResults.forEach(r => r.draw(context));
+		stars.forEach(s => {
+				s.update();
+				s.draw(context);
+		});
+		searchResults.forEach(r => {
+				r.update();
+				r.draw(context);
+		});
 
 		requestAnimationFrame(runAnimationLoop);
 }
@@ -99,7 +146,6 @@ function buildResults(results) {
 		const canvas = document.getElementById('result-view');
 		const context = canvas.getContext('2d');
 		
-		context.clearRect(0, 0, canvas.width, canvas.height);
 		const maxRelativeSize = getRelativeSize(results);
 		const centerX = canvas.width / 2;
 		const centerY = canvas.height / 2;
@@ -114,11 +160,5 @@ function buildResults(results) {
 				const y = centerY + (Math.sin(angle) * (centerY - maxRadius));
 				const sResult = new SearchResult(x, y, relativeRadius, possibleColors[Math.floor(Math.random()*possibleColors.length)], result.title);
 				searchResults.push(sResult);
-				sResult.draw(context);
 		});
-
-		if (!animationLoopStarted) {
-				animationLoopStarted = true;
-				runAnimationLoop();
-		}
 }
