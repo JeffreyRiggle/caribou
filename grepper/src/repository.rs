f571a4335f26d44557f743e7da9c7a39d3509a5a
Jeffrey Riggle <jeffreyriggle@gmail.com>
2024-12-18 21:40:53 -0500

    fix(grepper): SQL injection is bad

--- grepper/src/repository.rs
+++ grepper/src/repository.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use rusqlite::Connection;
+use rusqlite::{params, Connection};
 use crate::models::{AssetResult, DBAsset, GraphResult, GraphResultReponse, PageAssets};
 
 use super::models::{ResultsResponse, ResultData};
@@ -14,8 +14,8 @@ pub fn get_results(query: String) -> ResultsResponse {
         }
     };
     
-    let mut stmt = conn.prepare(format!("WITH res as (SELECT * FROM resources JOIN rank ON rank.url = resources.url WHERE Status = 'Processed' AND contentType = 'html' AND (summary LIKE '%{}%' OR description LIKE '%{}%') ORDER BY pageRank DESC) SELECT url, title, summary, description FROM res", query, query).as_str()).unwrap();
-    let rows = stmt.query_map([], |row| {
+    let mut stmt = conn.prepare("WITH res as (SELECT * FROM resources JOIN rank ON rank.url = resources.url WHERE Status = 'Processed' AND contentType = 'html' AND (summary LIKE ?1 OR description LIKE ?1) ORDER BY pageRank DESC) SELECT url, title, summary, description FROM res").unwrap();
+    let rows = stmt.query_map(params![format!("%{}%", query)], |row| {
         Ok(ResultData {
             url: row.get(0)?,
             title: row.get(1)?,
@@ -43,8 +43,8 @@ pub fn get_graph_result(url: String) -> GraphResult {
         }
     };
 
-    let mut stmt = conn.prepare(format!("WITH res as (SELECT * FROM resources JOIN rank ON rank.url = resources.url WHERE resources.url='{}') SELECT url, title, summary, pageRank FROM res", url).as_str()).unwrap();
-    let mut rows = stmt.query_map([], |row| {
+    let mut stmt = conn.prepare("WITH res as (SELECT * FROM resources JOIN rank ON rank.url = resources.url WHERE resources.url=?1) SELECT url, title, summary, pageRank FROM res").unwrap();
+    let mut rows = stmt.query_map(params![url.clone()], |row| {
         Ok(GraphResult {
             url: row.get(0)?,
             title: row.get(1)?,
@@ -63,8 +63,8 @@ pub fn get_graph_result(url: String) -> GraphResult {
         }
     };
 
-    let mut stmt = conn.prepare(format!("SELECT sourceUrl, targetUrl FROM links WHERE sourceUrl='{}'", row.url).as_str()).unwrap();
-    let mut link_rows = stmt.query([]).unwrap();
+    let mut stmt = conn.prepare("SELECT sourceUrl, targetUrl FROM links WHERE sourceUrl=?1").unwrap();
+    let mut link_rows = stmt.query(params![row.url.clone()]).unwrap();
     let mut target_links = Vec::new(); 
     while let Some(row) = link_rows.next().unwrap() {
         target_links.push(row.get(1).unwrap());
@@ -88,8 +88,8 @@ pub fn get_graph_results(query: String) -> GraphResultReponse {
         }
     };
     
-    let mut stmt = conn.prepare(format!("WITH res as (SELECT * FROM resources JOIN rank ON rank.url = resources.url WHERE Status = 'Processed' AND contentType = 'html' AND (summary LIKE '%{}%' OR description LIKE '%{}%') ORDER BY pageRank DESC) SELECT url, title, summary, pageRank FROM res", query, query).as_str()).unwrap();
-    let rows = stmt.query_map([], |row| {
+    let mut stmt = conn.prepare("WITH res as (SELECT * FROM resources JOIN rank ON rank.url = resources.url WHERE Status = 'Processed' AND contentType = 'html' AND (summary LIKE ?1 OR description LIKE ?1) ORDER BY pageRank DESC) SELECT url, title, summary, pageRank FROM res").unwrap();
+    let rows = stmt.query_map(params![format!("%{}%", query)], |row| {
         Ok(GraphResult {
             url: row.get(0)?,
             title: row.get(1)?,
@@ -102,8 +102,8 @@ pub fn get_graph_results(query: String) -> GraphResultReponse {
     let mut result = Vec::new();
     for domain_result in rows {
         let dresult = domain_result.unwrap();
-        let mut stmt = conn.prepare(format!("SELECT sourceUrl, targetUrl FROM links WHERE sourceUrl='{}'", dresult.url).as_str()).unwrap();
-        let mut link_rows = stmt.query([]).unwrap();
+        let mut stmt = conn.prepare("SELECT sourceUrl, targetUrl FROM links WHERE sourceUrl=?1").unwrap();
+        let mut link_rows = stmt.query(params![dresult.url.clone()]).unwrap();
         let mut target_links = Vec::new(); 
         while let Some(row) = link_rows.next().unwrap() {
             match row.get(1) {
@@ -134,8 +134,8 @@ pub fn get_assets(url: String) -> PageAssets {
         }
     };
 
-    let mut stmt = conn.prepare(format!("SELECT url, path, contentType from links JOIN resources ON links.targetUrl = resources.url WHERE links.sourceUrl = '{}'  AND resources.contentType != 'html'", url).as_str()).unwrap();
-    let rows = stmt.query_map([], |row| {
+    let mut stmt = conn.prepare("SELECT url, path, contentType from links JOIN resources ON links.targetUrl = resources.url WHERE links.sourceUrl = ?1  AND resources.contentType != 'html'").unwrap();
+    let rows = stmt.query_map(params![url], |row| {
         Ok(DBAsset {
             url: row.get(0)?,
             path: row.get(1)?,
@@ -171,8 +171,8 @@ pub fn get_page_data(url: String) -> DBAsset {
         }
     };
 
-    let mut stmt = conn.prepare(format!("select url, path, contentType from resources where url = '{}'", url).as_str()).unwrap();
-    let mut rows = stmt.query_map([], |row| {
+    let mut stmt = conn.prepare("select url, path, contentType from resources where url = ?1").unwrap();
+    let mut rows = stmt.query_map(params![url], |row| {
         Ok(DBAsset {
             url: row.get(0)?,
             path: row.get(1)?,
