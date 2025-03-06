3ff9d759046a6f777778d06fee585fee759e6f90
Jeffrey Riggle <jeffreyriggle@gmail.com>
2025-03-05 22:00:57 -0500

    fix(grepper): Make sure that postgres implmentation actually works

--- grepper/src/result_repository.rs
+++ grepper/src/result_repository.rs
@@ -156,22 +156,137 @@ impl ResultRepository for SQLiteConnection {
 
 impl ResultRepository for PostgresConnection {
     fn get_results(&mut self, query: String) -> ResultsResponse {
-        todo!()
+        let mut result = Vec::new();
+        let query_string = "
+WITH res as (
+	SELECT resources.url, resources.title, resources.summary, resources.description FROM resources
+	JOIN rank 
+	ON rank.url = resources.url WHERE Status = 'Processed' 
+	AND contentType = 'html' 
+	AND (summary LIKE $1 OR description LIKE $1)
+	ORDER BY pageRank desc
+)
+SELECT url, title, summary, description FROM res";
+
+        for domain_result in self.client.query(query_string, &[&format!("%{}%", query)]).unwrap() {
+            result.push(ResultData {
+                url: domain_result.get(0),
+                title: domain_result.get(1),
+                description: domain_result.get(2),
+                summary: domain_result.get(3)
+            });
+        }
+
+        ResultsResponse {
+            results: result
+        }
     }
 
     fn get_graph_result(&mut self, url: String) -> GraphResult {
-        todo!()
+        let query_string = "
+WITH res as (
+	SELECT resources.url, resources.title, resources.summary, rank.pagerank  
+	FROM resources 
+	JOIN rank 
+	ON rank.url = resources.url 
+	WHERE resources.url=$1
+) SELECT url, title, summary, pageRank FROM res";
+
+        let row = self.client.query_one(query_string, &[&url]).unwrap();
+        let graph_result = GraphResult {
+            url: row.get(0),
+            title: row.get(1),
+            summary: row.get(2),
+            rank: row.get(3),
+            links: Vec::new()
+        };
+
+        let mut target_links = Vec::new(); 
+        for link_result in self.client.query("SELECT targetUrl FROM links WHERE sourceUrl=$1", &[&graph_result.url]).unwrap() {
+            target_links.push(link_result.get(0));
+        }
+
+        GraphResult {
+            url: graph_result.url,
+            title: graph_result.title,
+            summary: graph_result.summary,
+            rank: graph_result.rank,
+            links: target_links
+        }
     }
 
     fn get_graph_results(&mut self, query: String) -> GraphResultReponse {
-        todo!()
+        let mut result = Vec::new();
+        let query_string = "
+WITH res as (
+	SELECT resources.url, resources.title, resources.summary , rank.pagerank 
+	FROM resources 
+	JOIN rank 
+	ON rank.url = resources.url 
+	WHERE Status = 'Processed' 
+	AND contentType = 'html' 
+	AND (summary LIKE $1 OR description LIKE $1)
+	ORDER BY pageRank desc
+) 
+SELECT url, title, summary, pageRank FROM res";
+
+        for domain_result in self.client.query(query_string, &[&format!("%{}%", query)]).unwrap() {
+            let dresult = GraphResult {
+                url: domain_result.get(0),
+                title: domain_result.get(1),
+                summary: domain_result.get(2),
+                rank: domain_result.get(3),
+                links: Vec::new()
+            };
+
+            let mut target_links = Vec::new();
+            for link in self.client.query("SELECT targetUrl FROM links WHERE sourceUrl=$1", &[&dresult.url]).unwrap() {
+                target_links.push(link.get(0));
+            }
+            result.push(GraphResult {
+                url: dresult.url,
+                title: dresult.title,
+                summary: dresult.summary,
+                rank: dresult.rank,
+                links: target_links
+            });
+        }
+
+        GraphResultReponse {
+            results: result
+        }
     }
 
     fn get_assets(&mut self, url: String) -> PageAssets {
-        todo!()
+        let mut resulting_assets = Vec::new();
+        for row in self.client.query("SELECT url, path, contentType from links JOIN resources ON links.targetUrl = resources.url WHERE links.sourceUrl = $1  AND resources.contentType != 'html'", &[&url]).unwrap() {
+            let resulting_row = DBAsset {
+                url: row.get(0),
+                path: row.get(1),
+                content_type: row.get(2)
+            };
+            let metadata = fs::metadata(resulting_row.path).unwrap();
+            let file_size = metadata.len();
+
+            resulting_assets.push(AssetResult {
+                url: resulting_row.url,
+                bytes: file_size,
+                content_type: resulting_row.content_type
+            })
+        }
+
+        PageAssets {
+            assets: resulting_assets
+        }
     }
 
     fn get_page_data(&mut self, url: String) -> DBAsset {
-        todo!()
+        let result= self.client.query_one("select url, path, contentType from resources where url = $1", &[&url]).unwrap();
+
+        DBAsset {
+            url: result.get(0),
+            path: result.get(1),
+            content_type: result.get(2)
+        }
     }
 }
